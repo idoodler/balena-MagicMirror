198062254d8fc72ee568426227d5d01c1766773f
idoodler <me@idoodler.de>
2019-07-18 21:19:34 +0200

    Fixed randome service restarts

--- MagicMirror/config/config.js
+++ MagicMirror/config/config.js
@@ -9,18 +9,9 @@
  */
 
 var config = {
-    address: "localhost", // Address to listen on, can be:
-                          // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-                          // - another specific IPv4/6 to listen on a specific interface
-                          // - "", "0.0.0.0", "::" to listen on any interface
-                          // Default, when address config is left out, is "localhost"
+    address: "",
     port: 8080,
-    ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
-                                                           // or add a specific IPv4 of 192.168.1.5 :
-                                                           // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-                                                           // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-                                                           // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
-
+    ipWhitelist: [],
     language: "en",
     timeFormat: 24,
     units: "metric",
