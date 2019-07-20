aa778af58d7ed483afb39e7817b2a621b2457289
idoodler <me@idoodler.de>
2019-07-20 12:49:05 +0200

    Updated config.js

--- MagicMirror/config/config.js
+++ MagicMirror/config/config.js
@@ -9,9 +9,18 @@
  */
 
 var config = {
-    address: "",
+    address: "localhost", // Address to listen on, can be:
+                          // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+                          // - another specific IPv4/6 to listen on a specific interface
+                          // - "", "0.0.0.0", "::" to listen on any interface
+                          // Default, when address config is left out, is "localhost"
     port: 8080,
-    ipWhitelist: [],
+    ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
+                                                           // or add a specific IPv4 of 192.168.1.5 :
+                                                           // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+                                                           // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+                                                           // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+
     language: "en",
     timeFormat: 24,
     units: "metric",
@@ -36,8 +45,7 @@ var config = {
                 calendars: [
                     {
                         symbol: "calendar-check",
-                        url: "webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics"
-                    }
+                        url: "webcal://www.calendarlabs.com/ical-calendar/ics/76/US_Holidays.ics"					}
                 ]
             }
         },
@@ -50,7 +58,7 @@ var config = {
             position: "top_right",
             config: {
                 location: "New York",
-                locationID: "",  //ID from http://bulk.openweathermap.org/sample/; unzip the gz file and find your city
+                locationID: "",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
                 appid: "YOUR_OPENWEATHER_API_KEY"
             }
         },
@@ -60,7 +68,7 @@ var config = {
             header: "Weather Forecast",
             config: {
                 location: "New York",
-                locationID: "5128581",  //ID from https://openweathermap.org/city
+                locationID: "5128581",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
                 appid: "YOUR_OPENWEATHER_API_KEY"
             }
         },
@@ -75,7 +83,9 @@ var config = {
                     }
                 ],
                 showSourceTitle: true,
-                showPublishDate: true
+                showPublishDate: true,
+                broadcastNewsFeeds: true,
+                broadcastNewsUpdates: true
             }
         },
     ]
